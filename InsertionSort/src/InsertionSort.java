32e34983c3822982519b94f472e502f3bec6d607
Suqu13 <jgarstka3@gmail.com>
2018-04-10 22:12:46 +0200

    InsertionSort

---
+++ InsertionSort/src/InsertionSort.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class InsertionSort {
+    private ArrayList<Integer> list;
+
+    public InsertionSort(String fileName) {
+        list = new ArrayList<Integer>();
+        String tab[];
+        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+            while ((line = reader.readLine()) != null) {
+                tab = line.split(",");
+                for (int i = 0; i < tab.length; i++) {
+                    list.add(Integer.parseInt(tab[i]));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void InsertionSort() {
+        int start = 0;
+        while (start < list.size() - 1) {
+            int i = start;
+            int j = start + 1;
+            while (i >= 0) {
+                if (list.get(i) > list.get(j))
+                    Collections.swap(list, j, i);
+                j--;
+                i--;
+            }
+            start++;
+        }
+    }
+
+    public void show() {
+        for (Integer i : list) {
+            System.out.print(" " + i + " ");
+        }
+        System.out.println("\n");
+    }
+
+
+}
---
+++ InsertionSort/src/TESTER.java
@@ -0,0 +1,8 @@
+public class TESTER {
+    public static void main(String[] args) {
+        InsertionSort dane = new InsertionSort("z2data1.csv");
+        dane.show();
+        dane.InsertionSort();
+        dane.show();
+    }
+}
